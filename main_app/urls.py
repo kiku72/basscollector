b68e6e4bf559c3a86aa3be5a798b30fec3e59089
kiku72 <128595283+kiku72@users.noreply.github.com>
2023-08-13 20:02:27 +1000

    Set up home, about and wishlist pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,2 +1,8 @@
-from django.shortcuts import render
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('basses/', views.bass_index, name="bass_index")
+]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 basses = [
-    {'make':'G&L', 'model':'SB2', 'scale':'34"', 'color': 'sonic blue', 'construction': 'Poplar with Maple or Rosewood fretboard' }
-    {'make':'Fender', 'model':'Jazz Bass', 'scale':'34"', 'color': 'sunburst', 'construction': 'Basswood body and maple neck' }
-    {'make':'Yamaha', 'model':'TRBX304', 'scale':'34"', 'color': 'white', 'construction': 'Mahogany body and maple/mahogany neck' }
+    {'make':'G&L', 'model':'SB2', 'scale':'34"', 'color': 'Sonic Blue', 'construction': 'Poplar body and maple/rosewood neck' },
+    {'make':'Fender', 'model':'Jazz Bass', 'scale':'34"', 'color': 'Sunburst', 'construction': 'Basswood body and maple neck' },
+    {'make':'Yamaha', 'model':'TRBX304', 'scale':'34"', 'color': 'Arctic White', 'construction': 'Mahogany body and maple/mahogany neck' },
 ]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+#we need to pass data, which we do with objects/dicts. this goes to the template!
+def bass_index(request):
+    return render(request, 'basses/index.html', {
+        'basses' : basses
+    })

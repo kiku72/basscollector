8b056ad5e75a69c92034ace13cf37b4727b80b3f
kiku72 <128595283+kiku72@users.noreply.github.com>
2023-08-13 17:57:24 +1000

    Initial commit

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+basses = [
+    {'make':'G&L', 'model':'SB2', 'scale':'34"', 'color': 'sonic blue', 'construction': 'Poplar with Maple or Rosewood fretboard' }
+    {'make':'Fender', 'model':'Jazz Bass', 'scale':'34"', 'color': 'sunburst', 'construction': 'Basswood body and maple neck' }
+    {'make':'Yamaha', 'model':'TRBX304', 'scale':'34"', 'color': 'white', 'construction': 'Mahogany body and maple/mahogany neck' }
+]
